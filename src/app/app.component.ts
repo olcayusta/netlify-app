304fa9a72431b358e493fca50cd29d0ab0d9fc86
olcayusta <olcayusta02@gmail.com>
2021-03-08 17:57:18 +0300

    update for vercel app 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'Hello, VERCEL!';
+  title = 'Hello, VERCEL 2!';
 }
